481c1f267c8c1c180385ac0258553437c189a8c1
hbs990n <2392159565@qq.com>
2026-06-17 16:22:27 +0800

    fix cmp atoi for Python 3 compatibility

--- tools/dct/obj/Md1EintObj.py
+++ tools/dct/obj/Md1EintObj.py
@@ -18,7 +18,7 @@ import xml.dom.minidom
 
 from utility import util
 from utility.util import sorted_key
-from ModuleObj import ModuleObj
+from .ModuleObj import ModuleObj
 from data.Md1EintData import Md1EintData
 from utility.util import LogLevel
 
@@ -52,7 +52,7 @@ class Md1EintObj(ModuleObj):
         try:
             for node in nodes:
                 if node.nodeType == xml.dom.Node.ELEMENT_NODE:
-                    if cmp(node.nodeName, 'count') == 0:
+                    if node.nodeName == 'count':
                         self.__count = node.childNodes[0].nodeValue
                         continue
 
@@ -121,7 +121,7 @@ class Md1EintObj(ModuleObj):
         count = 0
         for key in sorted_key(list(ModuleObj.get_data(self).keys())):
             value = ModuleObj.get_data(self)[key]
-            if cmp(value.get_varName(), 'NC') == 0:
+            if value.get_varName() == 'NC':
                 continue
             num = key[4:]
             count += 1
@@ -146,7 +146,7 @@ class Md1EintObj(ModuleObj):
         gen_str += '''&eintc {\n'''
         for key in sorted_key(list(ModuleObj.get_data(self).keys())):
             value = ModuleObj.get_data(self)[key]
-            if cmp(value.get_varName(), 'NC') == 0:
+            if value.get_varName() == 'NC':
                 continue
             num = key[4:]
             gen_str += '''\t%s@%s {\n''' %(value.get_varName(), num)
@@ -156,17 +156,17 @@ class Md1EintObj(ModuleObj):
             polarity = value.get_polarity()
             sensitive = value.get_sensitiveLevel()
 
-            if cmp(polarity, 'High') == 0 and cmp(sensitive, 'Edge') == 0:
+            if polarity == 'High' and sensitive == 'Edge':
                 type = 1
-            elif cmp(polarity, 'Low') == 0 and cmp(sensitive, 'Edge') == 0:
+            elif polarity == 'Low' and sensitive == 'Edge':
                 type = 2
-            elif cmp(polarity, 'High') == 0 and cmp(sensitive, 'Level') == 0:
+            elif polarity == 'High' and sensitive == 'Level':
                 type = 4
-            elif cmp(polarity, 'Low') == 0 and cmp(sensitive, 'Level') == 0:
+            elif polarity == 'Low' and sensitive == 'Level':
                 type = 8
 
             gen_str += '''\t\tinterrupts = <%s %d>;\n''' %(num, type)
-            gen_str += '''\t\tdebounce = <%s %d>;\n''' %(num, (string.atoi(value.get_debounceTime()))*1000)
+            gen_str += '''\t\tdebounce = <%s %d>;\n''' %(num, (int(value.get_debounceTime()))*1000)
             gen_str += '''\t\tdedicated = <%s %d>;\n''' %(num, int(value.get_dedicatedEn()))
             if self.__bSrcPinEnable:
                 gen_str += '''\t\tsrc_pin = <%s %s>;\n''' %(num, self.__srcPin[value.get_srcPin()])
@@ -196,7 +196,7 @@ class Md1EintObj_MT6739(Md1EintObj):
         gen_str = ''
         for key in sorted_key(list(ModuleObj.get_data(self).keys())):
             value = ModuleObj.get_data(self)[key]
-            if cmp(value.get_varName(), 'NC') == 0:
+            if value.get_varName() == 'NC':
                 continue
             num = key[4:]
             gen_str += '''&%s {\n''' % (value.get_varName().lower())
@@ -206,17 +206,17 @@ class Md1EintObj_MT6739(Md1EintObj):
             polarity = value.get_polarity()
             sensitive = value.get_sensitiveLevel()
 
-            if cmp(polarity, 'High') == 0 and cmp(sensitive, 'Edge') == 0:
+            if polarity == 'High' and sensitive == 'Edge':
                 type = 1
-            elif cmp(polarity, 'Low') == 0 and cmp(sensitive, 'Edge') == 0:
+            elif polarity == 'Low' and sensitive == 'Edge':
                 type = 2
-            elif cmp(polarity, 'High') == 0 and cmp(sensitive, 'Level') == 0:
+            elif polarity == 'High' and sensitive == 'Level':
                 type = 4
-            elif cmp(polarity, 'Low') == 0 and cmp(sensitive, 'Level') == 0:
+            elif polarity == 'Low' and sensitive == 'Level':
                 type = 8
 
             gen_str += '''\tinterrupts = <%s %d>;\n''' % (num, type)
-            gen_str += '''\tdebounce = <%s %d>;\n''' % (num, (string.atoi(value.get_debounceTime())) * 1000)
+            gen_str += '''\tdebounce = <%s %d>;\n''' % (num, (int(value.get_debounceTime())) * 1000)
             gen_str += '''\tdedicated = <%s %d>;\n''' % (num, int(value.get_dedicatedEn()))
             if self.get_srcPinEnable():
                 gen_str += '''\tsrc_pin = <%s %s>;\n''' % (num, self.get_srcPin()[value.get_srcPin()])
